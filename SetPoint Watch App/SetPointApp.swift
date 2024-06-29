039e88e67d79c866fb2195c50d06d8fc5e4fb409
Marco Marrocu <marrocumarcozaggi@gmail.com>
2024-06-29 11:53:56 +0200

    Initial Commit

---
+++ SetPoint Watch App/SetPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetPointApp.swift
+//  SetPoint Watch App
+//
+//  Created by marrocumarco on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SetPoint_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SetPoint Watch AppUITests/SetPoint_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SetPoint_Watch_AppUITestsLaunchTests.swift
+//  SetPoint Watch AppUITests
+//
+//  Created by marrocumarco on 29/06/2024.
+//
+
+import XCTest
+
+final class SetPoint_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

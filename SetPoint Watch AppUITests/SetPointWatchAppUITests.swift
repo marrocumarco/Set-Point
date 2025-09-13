791b6c15180d35e27260afb262c44aa61833a916
Marco Marrocu <marrocumarcozaggi@gmail.com>
2025-09-13 17:51:43 +0200

    removed swiftLint from spm to use the one installed from homebrew

--- SetPoint Watch AppUITests/SetPointWatchAppUITests.swift
+++ SetPoint Watch AppUITests/SetPointWatchAppUITests.swift
@@ -1,5 +1,5 @@
 //
-//  SetPoint_Watch_AppUITests.swift
+//  SetPointWatchAppUITests.swift
 //  SetPoint Watch AppUITests
 //
 //  Created by marrocumarco on 29/06/2024.
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class SetPoint_Watch_AppUITests: XCTestCase {
+final class SetPointWatchAppUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.

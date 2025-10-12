7f67dbebc03f5dce9b43074a06b9700e24e310db
Marco Marrocu <marrocumarcozaggi@gmail.com>
2025-10-12 19:09:41 +0200

    linked match view with view model

--- SetPoint Watch App/Presentation/ContentView.swift
+++ SetPoint Watch App/Presentation/ContentView.swift
@@ -25,57 +25,74 @@ struct PlayersRow: View {
     @Environment(MatchViewModel.self) private var matchViewModel: MatchViewModel
     var body: some View {
         HStack {
-            Text(matchViewModel.matchUseCase.player1Name)
+            Text(matchViewModel.player1Name)
             Spacer()
-            Text(matchViewModel.matchUseCase.player2Name)
+            Text(matchViewModel.player2Name)
         }
     }
 }
 
 struct GamesRow: View {
+    @Environment(MatchViewModel.self) private var matchViewModel: MatchViewModel
     var body: some View {
         HStack {
-            Text("3")
+            Text(matchViewModel.player1NumberOfGames.description)
             Spacer()
-            Text("Games")
+            Text(matchViewModel.gamesCaption)
             Spacer()
-            Text("2")
+            Text(matchViewModel.player2NumberOfGames.description)
         }
     }
 }
 
 struct SetsRow: View {
+    @Environment(MatchViewModel.self) private var matchViewModel: MatchViewModel
     var body: some View {
         HStack {
-            Text("1")
+            Text(matchViewModel.player1NumberOfSets.description)
             Spacer()
-            Text("Sets")
+            Text(matchViewModel.setsCaption)
             Spacer()
-            Text("0")
+            Text(matchViewModel.player2NumberOfSets.description)
         }
     }
 }
 
 struct ScoreRow: View {
+    @Environment(MatchViewModel.self) private var matchViewModel: MatchViewModel
     var body: some View {
         HStack {
             Group {
-                Button("10") {}
-                Button(action: {}) {
+                Button(matchViewModel.player1PointsDescription) {
+                    matchViewModel.player1Scored()
+                }
+                Button(action: {
+                    Task {
+                        await matchViewModel.undo()
+                    }}) {
                     Image(systemName: "arrow.uturn.backward")
                 }.frame(width: 44)
-                Button("20") {}
+                    .disabled(!matchViewModel.canUndo)
+                Button(matchViewModel.player2PointsDescription) {
+                    matchViewModel.player2Scored()
+                }
             }.clipShape(.circle)
         }
     }
 }
 
 struct SettingsRow: View {
+    @Environment(MatchViewModel.self) private var matchViewModel: MatchViewModel
+
     var body: some View {
         HStack {
             Group {
-                Button(action: {}, label: { Image(systemName: "gear") }).frame(width: 44)
-                Button(action: {}, label: { Image(systemName: "restart") }).frame(width: 44)
+                Button(action: {
+                    matchViewModel.openSettings()
+                }, label: { Image(systemName: "gear") }).frame(width: 44)
+                Button(action: {
+                    matchViewModel.resetMatch()
+                }, label: { Image(systemName: "restart") }).frame(width: 44)
             }.clipShape(.circle)
         }
     }
--- SetPoint Watch App/Presentation/MatchViewModel.swift
+++ SetPoint Watch App/Presentation/MatchViewModel.swift
@@ -9,11 +9,72 @@ import Combine
 import Foundation
 
 @Observable
+@MainActor
 class MatchViewModel {
 
     internal init(matchUseCase: any MatchUseCase) {
         self.matchUseCase = matchUseCase
+        updateView()
     }
-    
-    let matchUseCase: any MatchUseCase
+
+    private(set) var player1Name: String = ""
+    private(set) var player2Name: String = ""
+    private(set) var player1PointsDescription: String = ""
+    private(set) var player2PointsDescription: String = ""
+    private(set) var player1NumberOfGames: String = ""
+    private(set) var player2NumberOfGames: String = ""
+    private(set) var player1NumberOfSets: String = ""
+    private(set) var player2NumberOfSets: String = ""
+    private(set) var gamesCaption: String = ""
+    private(set) var setsCaption: String = ""
+    private(set) var canUndo: Bool = false
+    private(set) var matchUseCase: any MatchUseCase
+
+    func undo() async {
+        do {
+            try await matchUseCase.undo()
+            updateView()
+        } catch {
+            print(error)
+        }
+    }
+
+    func player1Scored() {
+        Task {
+            await matchUseCase.pointWonByPlayerOne()
+            updateView()
+        }
+    }
+
+    func player2Scored() {
+        Task {
+            await matchUseCase.pointWonByPlayerTwo()
+            updateView()
+        }
+    }
+
+    func resetMatch() {
+        Task {
+            await matchUseCase.resetMatch() //TODO: - show alert before reset
+            updateView()
+        }
+    }
+
+    func openSettings() {
+        
+    }
+
+    private func updateView() {
+        player1Name = matchUseCase.player1Name
+        player2Name = matchUseCase.player2Name
+        player1PointsDescription = matchUseCase.player1PointsDescription
+        player2PointsDescription = matchUseCase.player2PointsDescription
+        player1NumberOfGames = matchUseCase.player1NumberOfGames.description
+        player2NumberOfGames = matchUseCase.player2NumberOfGames.description
+        player1NumberOfSets = matchUseCase.player1NumberOfSets.description
+        player2NumberOfSets = matchUseCase.player2NumberOfSets.description
+        gamesCaption = matchUseCase.gamesCaption
+        setsCaption = matchUseCase.setsCaption
+        canUndo = matchUseCase.canUndo
+    }
 }
